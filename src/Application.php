54a87c72fb1ddef6704b7382af56ee156f30c9f3
Klall <kristo.lall@khk.ee>
2017-08-31 11:12:09 +0300

    menu to switch routes

--- src/Application.php
+++ src/Application.php
@@ -2,22 +2,14 @@
 
 namespace BookingApp;
 
+use BookingApp\Controllers\CreateBookingController;
+use BookingApp\Controllers\ListBookingsController;
 use Silex\Application as SilexApplication;
 use Silex\Provider\DoctrineServiceProvider;
-use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\FormServiceProvider;
 use Silex\Provider\LocaleServiceProvider;
 use Silex\Provider\TranslationServiceProvider;
-
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TimeType;
-
+use Silex\Provider\TwigServiceProvider;
 
 /**
  * Custom Application class that hold our application specifix functionality.
@@ -26,41 +18,43 @@ class Application extends SilexApplication
 {
     public function __construct(array $values = [])
     {
-
         parent::__construct($values);
 
         $this->configureServices();
-        $this->createDBTables();    
+        $this->createDBTables();
         $this->configureControllers();
-
     }
 
-    private function configureServices() {
-
+    /**
+     * Config app options and register services.
+     */
+    private function configureServices()
+    {
         $this['debug'] = true;
 
         $this->register(new TwigServiceProvider(), [
             'twig.path' => __DIR__.'/../views',
-
         ]);
 
-                // Database configuration
+            // Database configuration
         $this->register(new DoctrineServiceProvider(), [
             'db.options' => [
-                'driver' => 'pdo_sqlite',
-                'path' => __DIR__.'/../database/app.db',
+            'driver' => 'pdo_sqlite',
+            'path' => __DIR__.'/../database/app.db',
             ],
         ]);
 
         $this->register(new FormServiceProvider());
-		$this->register(new LocaleServiceProvider());
-		$this->register(new TranslationServiceProvider(), 
-			['translator.domains' => [],
-		]);
+        $this->register(new LocaleServiceProvider());
+        $this->register(new TranslationServiceProvider(), ['translator.domains' => [],]);
     }
 
-    private function createDBTables() {
-    	        if (!$this['db']->getSchemaManager()->tablesExist('bookings')) {
+    /**
+     * Creates all needed tables to database if they don't exist.
+     */
+    private function createDBTables()
+    {
+        if (!$this['db']->getSchemaManager()->tablesExist('bookings')) {
             $this['db']->executeQuery("CREATE TABLE bookings (
                 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                 firstName VARCHAR(40) NOT NULL,
@@ -76,52 +70,24 @@ class Application extends SilexApplication
                 payingMethod VARCHAR(10) NOT NULL
             );");
         }
-
     }
 
-    private function configureControllers() {
+    /**
+     * Define all used routes and connect a route to its controller.
+     */
+    private function configureControllers()
+    {
+        $this
+            ->match('/bookings/create', new CreateBookingController(
+                $this['form.factory'],
+                $this['twig'],
+                $this['db']
+            ))
+            ->method('GET|POST')
+            ->bind('booking_form');
 
-        $this->get('/bookings/create', function () {
-            																		$form = $this['form.factory']->createBuilder(FormType::class)
-                ->add('firstName', TextType::class, ['required' => true])
-                ->add('lastName', TextType::class, ['required' => true])
-                ->add('phone', TextType::class, ['required' => true])
-                ->add('email', TextType::class, ['required' => false])
-                ->add('birthday', DateType::class, [
-                    'required' => true,
-                    'widget' => 'single_text',
-                    'format' => 'dd.MM.yyyy',
-                ])
-                ->add('startDate', DateType::class, [
-                    'required' => true,
-                    'widget' => 'single_text',
-                    'format' => 'dd.MM.yyyy',
-                ])
-                ->add('endDate', DateType::class, [
-                    'required' => true,
-                    'widget' => 'single_text',
-                    'format' => 'dd.MM.yyyy',
-                ])
-                ->add('arrivalTime', TimeType::class, ['required' => true])
-                ->add('nrOfPeople', IntegerType::class, ['required' => true])
-                ->add('payingMethod', ChoiceType::class, [
-                    'choices' => [
-                        'cash' => 'cash',
-                        'transfer' => 'transfer',
-                    ],
-                    'required' => true,
-                ])
-                ->add('additionalInformation', TextareaType::class, [
-                    'required' => false
-                ])
-                ->add('submit', SubmitType::class, ['label' => 'Send booking'])
-                ->getForm()
-            ;
-
-            return $this['twig']->render('form.html.twig', [
-                'form' => $form->createView()
-            ]);;
-
-        });
+        $this
+            ->get('/bookings', new ListBookingsController($this['db'], $this['twig']))
+            ->bind('booking_list');
     }
 }
